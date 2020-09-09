cafa617f5c838e864c4dc08a7a478551803e340b
Oreo-IceCream <59699218+oreo-icecream@users.noreply.github.com>
2020-09-09 18:33:29 +0200

    Push Main Script

---
+++ Main.py
@@ -0,0 +1,7 @@
+choice = input("How many time do you want to POG????: ")
+choice_number = int(choice)
+poggers = "POGGERS!"
+#} End of Variables
+if choice_number > 0:
+    for i in range(int(choice)):
+        print(poggers)
