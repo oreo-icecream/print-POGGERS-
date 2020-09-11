09b12a80a642cb41b6a9e4845116fd1ef21ca1b3
Oreo-IceCream <59699218+oreo-icecream@users.noreply.github.com>
2020-09-11 20:01:24 +0200

    Add CLI and error messages

--- Main.py
+++ Main.py
@@ -1,7 +1,26 @@
-choice = input("How many time do you want to POG????: ")
-choice_number = int(choice)
 poggers = "POGGERS!"
-#} End of Variables
-if choice_number > 0:
-    for i in range(int(choice)):
-        print(poggers)
+# End of Variables
+# CLI
+print("What do you want to do?")
+print("")
+print("")
+print("1. POGGERS")
+print("2. Copypastas")
+print("")
+cli_choice = input("Please select a option: ")
+if cli_choice == 1:
+    choice = input("How many time do you want to POG????: ")
+    choice_number = int(choice)
+    #Variables ^
+    if choice_number > 0:
+        for i in range(int(choice)):
+            print(poggers)
+    elif choice_number == 0:
+        print("You're a dumbass. I'm tired of dumbasss like who think I can print 0 POGGERS. Fuck off")
+
+    input('Press "Enter" to stop: ')
+elif cli_choice == 2:
+    print("Sorry! This isn't ready yet. :>")
+else:
+    print("What are you doimg?? You druggo!")
+
