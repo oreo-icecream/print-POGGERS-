f164091cceccb3da3c0246835d0bbebdd04de280
Oreo-IceCream <59699218+oreo-icecream@users.noreply.github.com>
2020-09-11 20:19:49 +0200

    Add "Copypasta" option and 2 copypastas

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 poggers = "POGGERS!"
 # End of Variables
 # CLI
@@ -7,6 +8,8 @@ print("")
 print("1. POGGERS")
 print("2. Copypastas")
 print("")
+
+
 cli_choice = input("Please select a option: ")
 if cli_choice == 1:
     choice = input("How many time do you want to POG????: ")
@@ -20,7 +23,17 @@ if cli_choice == 1:
 
     input('Press "Enter" to stop: ')
 elif cli_choice == 2:
-    print("Sorry! This isn't ready yet. :>")
+    print("What copypasta do you wand to see?")
+    print("")
+    print("")
+    print("1. Navy Seals")
+    print("2. Navy Seals but NSFW")
+    print("")
+    cli_choice = input("CHoose a option: ")
+    if cli_choice == 2:
+        print("What the fuck did you just fucking say about me, you little bitch. I’ll have you know my name is John, and I woke up this morning 5:30 sharp to the smell of wet pussy. I was getting a blowjob from two bitches (Shit was SO Cash), one was trying to fit my humongous 3 pound balls in her mouth while the other was choking halfway on my 18 and 3\8 inch dick. She started to squirt hard, she was convulsing and having 6 orgasms at the same time. I gave it to them and they were on the floor squirting like motherfucking fountains. Must have come about a quart of sperm and compressed air. Imagine your best orgasm, then multiply it by 35. I had to go to base camp so I front-flipped from my 14th floor barracks into my valet parked 2012 Ferrari (I got connexions). Pushed my shit to about 4 hundo (mph, mind you) and I was at base camp in no time. When I entered, I became a top sniper and was granted access to the entire arsenal of the USMC. I learned how to kill someone in over 700 different ways and was assigned to be the leader of a squad that will kill 300 terrorists using gorilla warfare tactics. Also did 6000 push-ups, 8000 sit-ups and bench-pressed 30 plates in 16 minutes. After basic training, I met a network of secret spies who will help me trace your IP address, while eating gold plated sushi and 15,000 dollar champagne. My unit got the rest of the day off and I became captain of our base’s football team and starter of the basketball team. I got straight A’s on the military entrance exams and received more awards. Meanwhile, you were jacking off to pictures on Facebook and naked drawn Japanese people. Went back in the Lambo to my barracks and now I am getting ready to go to sleep. I am going to graduate at the top of my class in the Navy Seals tomorrow and I want to look pretty much perfect for it. Don’t be a stranger and remember, I did more in one day than you will your entire life.")
+    elif cli_choice == 1:
+        print("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little clever comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
 else:
     print("What are you doimg?? You druggo!")
 
